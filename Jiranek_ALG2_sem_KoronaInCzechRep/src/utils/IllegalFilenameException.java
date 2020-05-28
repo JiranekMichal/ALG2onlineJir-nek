29465cfc92857d45e63c651376ba53bfd85b6c3b
michal.jiranek <michal.jiranek@tul.cz>
2020-05-28 22:21:11 +0200

    Recursion 2.1

---
+++ Jiranek_ALG2_sem_KoronaInCzechRep/src/utils/IllegalFilenameException.java
@@ -0,0 +1,14 @@
+
+package competition.utils;
+
+/**
+ *
+ * @author Michal Jiránek
+ */
+public class IllegalFilenameException extends IllegalArgumentException {
+
+    public IllegalFilenameException(String messege) {
+        super(messege);
+    }
+    
+}
